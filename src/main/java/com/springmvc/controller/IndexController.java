7f6bb41753d55c853c0b041ddd5f89e620632a66
hzh8934 <hzh8934@qq.com>
2018-12-20 17:14:00 +0800

    12312

---
+++ src/main/java/com/springmvc/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.springmvc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("home")
+    public String home(){
+
+
+        return "home";
+    }
+}
